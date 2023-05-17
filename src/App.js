6b540de9f1770b27cbb92b8e115260ead545affb
Myroslav Baibarza <tutuk6835@gmail.com>
2023-05-17 11:38:51 +0300

    Start Project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import './assets/global-style/reset.css'
+
+function App() {
+  return (
+    <div className='Add'>
+
+
+      {/* 
+      <Router>
+        <Routes>
+          <Route path="/" element={<PostsList />} />
+          <Route path="/todolist" element={<TodoList />} />
+          <Route path="/userlist" element={<UserList />} />
+        </Routes>
+      </Router>
+      */}
+    </div>
+  );
+}
+
+export default App;
